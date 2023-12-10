6e70a6b2d509008e3567390818ce97b9385dcd73
bitcat <kurohaj96@gmail.com>
2023-12-10 15:25:17 +0900

    del: フォーク元の欲しいもの以外削除

--- server.js
+++ server.js
@@ -4,12 +4,6 @@ const cors = require("cors");
 
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
-const postRoute = require("./routes/posts");
-const subjectRoute = require("./routes/subjects");
-const roomRoute = require("./routes/rooms");
-const teacherRoute = require("./routes/teachers");
-const classRoute = require("./routes/classes");
-const timetableRoute = require("./routes/timetable");
 
 const PORT = 4000;
 const mongoose = require("mongoose");
@@ -29,11 +23,5 @@ app.use(express.json());
 app.use(cors());
 app.use("/api/users",userRoute);
 app.use("/api/auth",authRoute);
-app.use("/api/posts",postRoute);
-app.use("/api/subjects",subjectRoute);
-app.use("/api/rooms",roomRoute);
-app.use("/api/teachers",teacherRoute);
-app.use("/api/classes",classRoute);
-app.use("/api/timetable",timetableRoute);
 
 app.listen(PORT,() => console.log("サーバーが起動しました"));
